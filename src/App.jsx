ad55b463b9f9ffd882d8322d746168a8dcb9503c
Martin Myhalyna <martinmyhalyna@gmail.com>
2024-04-07 00:09:16 +0200

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { requestImagesByQuery } from "./services/api";
+import SearchBar from "./components/SearchBar/SearchBar";
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "./components/ImageModal/ImageModal";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [images, setImages] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    async function fetchImagesByQuery() {
+      try {
+        setIsLoading(true);
+        const data = await requestImagesByQuery(query, page);
+        if (page === 1) {
+          setImages(data.results);
+        } else {
+          setImages((prevImages) => [...prevImages, ...data.results]);
+        }
+        setIsError(false);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    if (query.length > 0) {
+      fetchImagesByQuery();
+    } else {
+      setImages(null); // Reset images when query is empty
+    }
+  }, [query, page]);
+
+  const onSetSearchQuery = (searchQuery) => {
+    setQuery(searchQuery);
+    setPage(1);
+  };
+
+  const handleMoreImages = async () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const openModal = (imageUrl) => {
+    setModalIsOpen(true);
+    setSelectedImage(imageUrl);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setSelectedImage(null);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <SearchBar onSubmit={onSetSearchQuery} />
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {images && <ImageGallery images={images} onImageClick={openModal} />}
+      {images && <LoadMoreBtn onClick={handleMoreImages} />}
+      <ImageModal
+        isOpen={!!selectedImage}
+        image={selectedImage}
+        onRequestClose={closeModal}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,9 @@
+const LoadMoreBtn = ({ onClick }) => {
+  return (
+    <button onClick={onClick} type="button">
+      Load more
+    </button>
+  );
+};
+
+export default LoadMoreBtn;
